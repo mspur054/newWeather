1c648bff6b8426244da355eea086398c971e44c9
Matt <mspur054@uottawa.ca>
2016-11-30 19:54:23 -0500

    add weather icons
    Weather icons added  based on weather API
    Next step is cleaning up functions

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
 
-      $("#city").html("WHATUP");
+      $("#city").html("&deg");
 
+      //Tests to see if navigator will work.
       if (!navigator.geolocation) {
         $("#city").html("Your browser does not support the Weather API.");
         return;
       }
 
+      //Gets the weather once the person approves
       function getWeather(personLocation) {
 
         var latitude = personLocation.coords.latitude;
         var longitude = personLocation.coords.longitude;
-
+//var apiUrl = "https://api.wunderground.com/api/7d3df977a43df28b/geolookup/conditions/lang:EN/q/";
         var apiUrl = "https://api.wunderground.com/api/7d3df977a43df28b/geolookup/conditions/lang:EN/q/";
         apiUrl += latitude + "," + longitude + ".json";
         var location;
         var temp_c;
         var temp_f;
+
+        console.log(apiUrl);
 
         $.ajax({
             url: apiUrl,
@@ -25,12 +29,16 @@ $(document).ready(function() {
               location = parsed_json.location.nearby_weather_stations.pws.station[0].city;
               temp_c = parsed_json.current_observation.temp_c;
               temp_f = parsed_json.current_observation.temp_f;
+              iconHtml = "<h2><i class ='wi wi-" + parsed_json.current_observation.icon + "'></i>" + temp_c + "</h2>";
+              
+
               //alert("Current temperature in " + location + " is: " + temp_c);
               $("#city").html(location + " ");
               $("#temp").html(temp_c);
               $("#descriptor").html("&#8451");
-              
-              
+              $("#icon").html(iconHtml);
+
+
               $("#btnCelcius").on("click",
               function() {
                 $("#temp").html(temp_c);
